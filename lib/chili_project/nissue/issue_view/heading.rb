2c5cff46e6d196e3017650bc6efc39cf94d6f30e
Gregor Schmidt <ruby@schmidtwisser.de>
2011-05-10 18:32:53 +0200

    readding author information

--- lib/chili_project/nissue/issue_view/heading.rb
+++ lib/chili_project/nissue/issue_view/heading.rb
@@ -6,6 +6,12 @@ class ChiliProject::Nissue::IssueView::Heading < ChiliProject::Nissue::View
   end
 
   def render(t)
-    content_tag(:div, t.render_issue_subject_with_tree(@issue), :class => 'subject')
+    content_tag(:div, t.render_issue_subject_with_tree(@issue), :class => 'subject') +
+    content_tag(:p, [
+      t.authoring(@issue.created_on, @issue.author),
+      '. ',
+      @issue.created_on != @issue.updated_on ? l(:label_updated_time, t.time_tag(@issue.updated_on)) + '.' : ''
+    ], :class => 'author')
+
   end
 end
