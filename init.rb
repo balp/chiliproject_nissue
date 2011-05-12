d4b2b30ae1156d3c13f4eca6a126397e4495f509
Gregor Schmidt <ruby@schmidtwisser.de>
2011-05-12 17:28:32 +0200

    adding basic init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_nissue do
+  name 'ChiliProject Nissue'
+  author 'Gregor Schmidt'
+  description 'A plugin which acts as helper for other plugins'
+  version '0.1.0'
+end
