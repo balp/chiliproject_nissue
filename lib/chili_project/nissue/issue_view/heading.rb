7e40f5d78091e8ba9fa95abb6328cd4d587be80b
Gregor Schmidt <ruby@schmidtwisser.de>
2011-05-16 17:58:47 +0200

    splitting up method to enable selective extension from sub classes

--- lib/chili_project/nissue/issue_view/heading.rb
+++ lib/chili_project/nissue/issue_view/heading.rb
@@ -6,12 +6,18 @@ class ChiliProject::Nissue::IssueView::Heading < ChiliProject::Nissue::View
   end
 
   def render(t)
-    content_tag(:div, t.render_issue_subject_with_tree(@issue), :class => 'subject') +
+    render_issue_subject_with_tree(t) + render_author(t)
+  end
+
+  def render_issue_subject_with_tree(t)
+    content_tag(:div, t.render_issue_subject_with_tree(@issue), :class => 'subject')
+  end
+
+  def render_author(t)
     content_tag(:p, [
       t.authoring(@issue.created_on, @issue.author),
       '. ',
       @issue.created_on != @issue.updated_on ? l(:label_updated_time, t.time_tag(@issue.updated_on)) + '.' : ''
     ], :class => 'author')
-
   end
 end
