7be9750540a83a2de3488e8d7a9f3cf9e7a15a44
Gregor Schmidt <ruby@schmidtwisser.de>
2011-07-08 17:50:33 +0200

    Releasing 0.1.1
    fixing issue view for issues with associated changesets

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :chiliproject_nissue do
   name 'ChiliProject Nissue'
   author 'Gregor Schmidt'
   description 'A plugin which acts as helper for other plugins'
-  version '0.1.0'
+  version '0.1.1'
 end
